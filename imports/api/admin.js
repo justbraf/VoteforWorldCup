86c62d7b5024e1b617bde2898e2d2581e8bb1055
JustBraf <dwaynecorey+github@gmail.com>
2018-04-18 01:52:00 -0400

    flags added

--- imports/api/admin.js
+++ imports/api/admin.js
@@ -4,3 +4,26 @@ Template.adminConsole.events({
 		// alert("go teams");
 	}
 });
+
+Template.teamConsole.onCreated(function(){
+	this.teamFlag = new ReactiveVar("flag_white.png");
+});
+
+Template.teamConsole.helpers({
+	teamFlag: function(){
+		return Template.instance().teamFlag.get();
+	}
+});
+
+Template.teamConsole.events({
+	'change #teamFlagFile': function(event, instance){
+		// console.log("Filename: " + event.target.files[0].name);
+		// console.log("Size: " + event.target.files[0].size + " bytes");
+		// console.log("File type: " + event.target.files[0].type);
+		var imageName = event.target.files[0].name;
+		instance.teamFlag.set(imageName);
+		$("#teamNameField").val(imageName.replace(".png",""));
+	},
+	'click .js-saveTeam': function(){
+	}
+});
