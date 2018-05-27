89f6777e13cbceae5f059127c4c45677684e7838
justbraf <dwaynecorey+github@gmail.com>
2018-05-27 14:10:34 -0400

    login test 2.2

--- server/main.js
+++ server/main.js
@@ -13,3 +13,13 @@ Meteor.startup(() => {
 		});
 	});
 });
+
+Meteor.publish('userData', function () {
+  if (this.userId) {
+    return Meteor.users.find({ _id: this.userId }, {
+      fields: { services: 1}
+    });
+  } else {
+    this.ready();
+  }
+});
