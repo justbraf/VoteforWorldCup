fd975224b4ef9f0c0fa4ef3f2250f4bdd0ffb900
justbraf <dwaynecorey@gmail.com>
2022-11-15 00:22:59 -0400

    scripts to init db

---
+++ imports/startup/groupMatches.js
@@ -0,0 +1,28 @@
+groupMatches = [
+  {
+    group: "A",
+    matchDate: 1668970800000,
+    teamOne: 1,
+    teamTwo: 2
+  },
+  {
+    group: "B",
+    matchDate: 1669046400000,
+    teamOne: 1,
+    teamTwo: 2
+  },
+  {
+    group: "A",
+    matchDate: 1669057200000,
+    teamOne: 3,
+    teamTwo: 4
+  },
+  {
+    group: "B",
+    matchDate: 1669068000000,
+    teamOne: 3,
+    teamTwo: 4
+  }
+]
+
+// Group #, timestamp, team1, team2
---
+++ imports/startup/groups.js
@@ -0,0 +1,180 @@
+Groups = [
+  {
+    grpName: "A",
+    group: [
+      {
+        groupId: 1,
+        team: "Qatar"
+      },
+      {
+        groupId: 2,
+        team: "Ecuador"
+      },
+      {
+        groupId: 3,
+        team: "Senegal"
+      },
+      {
+        groupId: 4,
+        team: "Netherlands"
+      }
+    ]
+  },
+  {
+    grpName: "B",
+    group: [
+      {
+        groupId: 1,
+        team: "England"
+      },
+      {
+        groupId: 2,
+        team: "IR Iran"
+      },
+      {
+        groupId: 3,
+        team: "USA"
+      },
+      {
+        groupId: 4,
+        team: "Wales"
+      }
+    ]
+  },
+  {
+    grpName: "C",
+    group: [
+      {
+        groupId: 1,
+        team: "Argentina"
+      },
+      {
+        groupId: 2,
+        team: "Saudi Arabia"
+      },
+      {
+        groupId: 3,
+        team: "Mexico"
+      },
+      {
+        groupId: 4,
+        team: "Poland"
+      }
+    ]
+  },
+  {
+    grpName: "D",
+    group: [
+      {
+        groupId: 1,
+        team: "France"
+      },
+      {
+        groupId: 2,
+        team: "Denmark"
+      },
+      {
+        groupId: 3,
+        team: "Tunisia"
+      },
+      {
+        groupId: 4,
+        team: "Australia"
+      }
+    ]
+  },
+  {
+    grpName: "E",
+    group: [
+      {
+        groupId: 1,
+        team: "Spain"
+      },
+      {
+        groupId: 2,
+        team: "Germany"
+      },
+      {
+        groupId: 3,
+        team: "Japan"
+      },
+      {
+        groupId: 4,
+        team: "Costa Rica"
+      }
+    ]
+  },
+  {
+    grpName: "F",
+    group: [
+      {
+        groupId: 1,
+        team: "Belgium"
+      },
+      {
+        groupId: 2,
+        team: "Canada"
+      },
+      {
+        groupId: 3,
+        team: "Morocco"
+      },
+      {
+        groupId: 4,
+        team: "Croatia"
+      }
+    ]
+  },
+  {
+    grpName: "G",
+    group: [
+      {
+        groupId: 1,
+        team: "Brazil"
+      },
+      {
+        groupId: 2,
+        team: "Serbia"
+      },
+      {
+        groupId: 3,
+        team: "Switzerland"
+      },
+      {
+        groupId: 4,
+        team: "Cameroon"
+      }
+    ]
+  },
+  {
+    grpName: "H",
+    group: [
+      {
+        groupId: 1,
+        team: "Portugal"
+      },
+      {
+        groupId: 2,
+        team: "Ghana"
+      },
+      {
+        groupId: 3,
+        team: "Uruguay"
+      },
+      {
+        groupId: 4,
+        team: "Korea Republic"
+      }
+    ]
+  }
+]
+
+// ===============================================
+
+Groups.forEach(el => {
+  console.log(el.grpName)
+  // console.log(el.group)
+  el.group.forEach(k => {
+    console.log(k)
+  })
+});
