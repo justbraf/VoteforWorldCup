a521d55fd70b25d4bf5199197c45f87f554f466b
justbraf <dwaynecorey@gmail.com>
2022-11-17 02:21:35 -0400

    populate two DBs wiht initial data, if empty

--- server/main.js
+++ server/main.js
@@ -1,16 +1,37 @@
 import { Meteor } from 'meteor/meteor';
 import '../lib/collections.js';
 import '../imports/startup/accounts-external.js';
+import '../imports/startup/groups.js'
+import '../imports/startup/groupMatches.js'
+
+let teamInsert = (args) => {
+	teamsdb.insert(args);
+}
+
+let fixturesInsert = (args) => {
+	fixturesdb.insert(args);
+}
 
 Meteor.startup(() => {
-	// code to run on server at startup	
+	// inject data if db is new
+	if (teamsdb.find().count() == 0) {
+		groups.forEach(team => {
+			teamInsert(team)
+		});
+	}
+	if (fixturesdb.find().count() == 0) {
+		groupMatches.forEach(fixture => {
+			fixturesInsert(fixture)
+		});
+	}
+	// console.log("Fixtures Found", fixturesdb.find().count())
 
 	Meteor.publish('teams', function () {
 		return teamsdb.find({});
 	});
 
 	Meteor.publish('matches', function () {
-		return matchesdb.find({});
+		return fixturesdb.find({});
 	});
 
 	Meteor.publish('ranks', function () {
@@ -31,11 +52,11 @@ Meteor.startup(() => {
 });
 
 Meteor.publish('userData', function () {
-  if (this.userId) {
-    return Meteor.users.find({}, { //_id: this.userId
-      fields: { services: 1, "profile.name": 1}
-    });
-  } else {
-    this.ready();
-  }
+	if (this.userId) {
+		return Meteor.users.find({}, { //_id: this.userId
+			fields: { services: 1, "profile.name": 1 }
+		});
+	} else {
+		this.ready();
+	}
 });
