0f31d2955ef3a98672127efed5abcebccc2093f3
justbraf <dwaynecorey@gmail.com>
2022-11-20 01:58:44 -0400

    test 1

--- imports/api/userLogin.js
+++ imports/api/userLogin.js
@@ -1,35 +1,47 @@
-// Template.userLogin.helpers({
-// 	noUser: function(){
-// 		if (!Meteor.userId())
-// 			return true;
-// 		else
-// 			return false;
-// 	},
-	// usersname: function(){
-	// 	if (!Meteor.userId())
-	// 		return "not logged in";
-	// 	else {		
-	// 		return Meteor.user().services.facebook.email;
-	// 	}
-	// }
-// });
+Template.userLogin.helpers({
+	// 	noUser: function(){
+	// 		if (!Meteor.userId())
+	// 			return true;
+	// 		else
+	// 			return false;
+	// 	},
+	username: () => {
+		if (!Meteor.userId())
+			return "not logged in"
+		let userData
+		if (Meteor.user().services)
+			//  || Meteor.user().services.facebook || Meteor.user().services.facebook.email)
+			userData = Meteor.user().services.facebook.email
+		if (!userData)
+			userData = Meteor.user().emails[0].address
+		if (!userData)
+			userData = "NOne name found"
+		console.log("Did it work", userData)
+		return userData
+	}
+})
 
-// Template.userLogin.events({
-// 	'click #login-buttons-facebook': function(e) {
-// 		e.preventDefault();
-// 		// console.log('attempting login');
-// 		Meteor.loginWithFacebook({requestPermissions: ['public_profile', 'email']}, function(err){
-// 				if (err) {
-// 						console.log('Handle errors here: ', err);
-// 						return;
-// 				}
-// 				// else {
-// 				// 	// console.log('redirect to Welcome');
-// 				// 	return Router.go('welcome');
-// 				// }
-// 		});
-// 	}
-// });
+Template.userLogin.events({
+	'click .js-signOut': () => {
+		teamsdb.remove({ _id: "489yjFLWFgcnYLRNR" })
+		teamsdb.remove({ _id: "CSdStaECf9nQMQdqP" })
+		Accounts.logout()
+	}
+	// 	'click #login-buttons-facebook': function(e) {
+	// 		e.preventDefault();
+	// 		// console.log('attempting login');
+	// 		Meteor.loginWithFacebook({requestPermissions: ['public_profile', 'email']}, function(err){
+	// 				if (err) {
+	// 						console.log('Handle errors here: ', err);
+	// 						return;
+	// 				}
+	// 				// else {
+	// 				// 	// console.log('redirect to Welcome');
+	// 				// 	return Router.go('welcome');
+	// 				// }
+	// 		});
+	// 	}
+})
 
 // Template.login.events({
 // 		'click .login-facebook': function(e) {
