5dcf3d65ad08b99883e59aad6acf4997d4165ec7
justbraf <dwaynecorey@gmail.com>
2022-11-20 03:10:07 -0400

    Upd UI

--- imports/api/groupList.js
+++ imports/api/groupList.js
@@ -1,26 +1,26 @@
 Template.groupList.helpers({
   GroupA: function () {
-    return teamsdb.find({ grpName: { $eq: "A" } }, { sort: { team: 1 } });
+    return teamsdb.find({ grpName: { $eq: "A" } }, { sort: { groupId: 1 } });
   },
   GroupB: function () {
-    return teamsdb.find({ grpName: { $eq: "B" } }, { sort: { team: 1 } });
+    return teamsdb.find({ grpName: { $eq: "B" } }, { sort: { groupId: 1 } });
   },
   GroupC: function () {
-    return teamsdb.find({ grpName: { $eq: "C" } }, { sort: { team: 1 } });
+    return teamsdb.find({ grpName: { $eq: "C" } }, { sort: { groupId: 1 } });
   },
   GroupD: function () {
-    return teamsdb.find({ grpName: { $eq: "D" } }, { sort: { team: 1 } });
+    return teamsdb.find({ grpName: { $eq: "D" } }, { sort: { groupId: 1 } });
   },
   GroupE: function () {
-    return teamsdb.find({ grpName: { $eq: "E" } }, { sort: { team: 1 } });
+    return teamsdb.find({ grpName: { $eq: "E" } }, { sort: { groupId: 1 } });
   },
   GroupF: function () {
-    return teamsdb.find({ grpName: { $eq: "F" } }, { sort: { team: 1 } });
+    return teamsdb.find({ grpName: { $eq: "F" } }, { sort: { groupId: 1 } });
   },
   GroupG: function () {
-    return teamsdb.find({ grpName: { $eq: "G" } }, { sort: { team: 1 } });
+    return teamsdb.find({ grpName: { $eq: "G" } }, { sort: { groupId: 1 } });
   },
   GroupH: function () {
-    return teamsdb.find({ grpName: { $eq: "H" } }, { sort: { team: 1 } });
+    return teamsdb.find({ grpName: { $eq: "H" } }, { sort: { groupId: 1 } });
   }
 });
--- imports/api/userLogin.js
+++ imports/api/userLogin.js
@@ -11,20 +11,27 @@ Template.userLogin.helpers({
 		let userData
 		if (Meteor.user().profile)
 			if (Meteor.user().profile.name)
-			userData = Meteor.user().profile.name
+				userData = Meteor.user().profile.name
 		if (!userData)
 			userData = Meteor.user().emails[0].address
 		if (!userData)
-			userData = "NOne name found"
-		console.log("Did it work", userData)
+			userData = "Anonymous"
 		return userData
 	}
 })
 
 Template.userLogin.events({
 	'click .js-signOut': () => {
-		teamsdb.remove({ _id: "489yjFLWFgcnYLRNR" })
-		teamsdb.remove({ _id: "CSdStaECf9nQMQdqP" })
+		teamsdb.insert({
+			grpName: "A",
+			groupId: 2,
+			team: "Ecuador"
+		});
+		teamsdb.insert({
+			grpName: "A",
+			groupId: 4,
+			team: "Netherlands"
+		});
 		Accounts.logout()
 	}
 	// 	'click #login-buttons-facebook': function(e) {
