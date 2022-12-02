1e251513ad234163b3264bada3fa1d1afaa41e63
justbraf <dwaynecorey@gmail.com>
2022-12-02 07:32:24 -0400

    patch 3

--- imports/api/groupList.js
+++ imports/api/groupList.js
@@ -1,27 +1,29 @@
+import { Template } from 'meteor/templating'
+
 Template.groupList.helpers({
   GroupA: function () {
-    return teamsdb.find({ grpName: { $eq: "A" } }, { sort: { groupId: 1 } });
+    return teamsdb.find({ grpName: { $eq: "A" } }, { sort: { points: -1, groupId: 1 } });
   },
   GroupB: function () {
-    return teamsdb.find({ grpName: { $eq: "B" } }, { sort: { groupId: 1 } });
+    return teamsdb.find({ grpName: { $eq: "B" } }, { sort: { points: -1, groupId: 1 } });
   },
   GroupC: function () {
-    return teamsdb.find({ grpName: { $eq: "C" } }, { sort: { groupId: 1 } });
+    return teamsdb.find({ grpName: { $eq: "C" } }, { sort: { points: -1, groupId: 1 } });
   },
   GroupD: function () {
-    return teamsdb.find({ grpName: { $eq: "D" } }, { sort: { groupId: 1 } });
+    return teamsdb.find({ grpName: { $eq: "D" } }, { sort: { points: -1, groupId: 1 } });
   },
   GroupE: function () {
-    return teamsdb.find({ grpName: { $eq: "E" } }, { sort: { groupId: 1 } });
+    return teamsdb.find({ grpName: { $eq: "E" } }, { sort: { points: -1, groupId: 1 } });
   },
   GroupF: function () {
-    return teamsdb.find({ grpName: { $eq: "F" } }, { sort: { groupId: 1 } });
+    return teamsdb.find({ grpName: { $eq: "F" } }, { sort: { points: -1, groupId: 1 } });
   },
   GroupG: function () {
-    return teamsdb.find({ grpName: { $eq: "G" } }, { sort: { groupId: 1 } });
+    return teamsdb.find({ grpName: { $eq: "G" } }, { sort: { points: -1, groupId: 1 } });
   },
   GroupH: function () {
-    return teamsdb.find({ grpName: { $eq: "H" } }, { sort: { groupId: 1 } });
+    return teamsdb.find({ grpName: { $eq: "H" } }, { sort: { points: -1, groupId: 1 } });
   },
   // showStats: function () {
   //   console.warn({ this: this })
